46a404343de3a6f68c5508ca84f1d1ae7a8b15f1
Edy <94276786+EdySnakee@users.noreply.github.com>
2023-10-19 06:09:30 -0600

    [fix] bugs

--- src/app/sistema_reclutamiento/catalogo-turnos/catalogo-turnos.component.ts
+++ src/app/sistema_reclutamiento/catalogo-turnos/catalogo-turnos.component.ts
@@ -5,33 +5,39 @@ import { Turno } from 'src/app/models/Turno';
 import { DetalleTurno } from 'src/app/models/DetalleTurno';
 import { MatTableDataSource } from '@angular/material/table';
 import Swal from 'sweetalert2';
-
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-catalogo-turnos',
   templateUrl: './catalogo-turnos.component.html',
   styleUrls: ['./catalogo-turnos.component.css'],
 })
-
-
 export class CatalogoTurnosComponent implements OnInit {
   public color = COLOR;
   public turnos: { [key: string]: any } | undefined;
   public detalle = new DetalleTurno(0, 0, '', '', 0);
-  public turno = new Turno(0, 5, '', '', 0, 0, 0, 0, 0, '', 1, 0, [this.detalle,]);
+  public turno = new Turno(0, 5, '', '', 0, 0, 0, 0, 0, '', 1, 0, [
+    this.detalle,
+  ]);
   public isChecked: any;
-  editando = false;
-  displayedColumns: string[] = ['clave', 'turno'];
-  dataSource = new MatTableDataSource();
+  public editando = false;
+  public displayedColumns: string[] = ['clave', 'turno'];
+  public dataSource = new MatTableDataSource();
+  public horaEntrada: any = '';
+  public turnoSeleccionado: any = '';
+  public editTurno : boolean = false;
+  public newTurno : boolean = false;
 
-  constructor(private turnosService: TurnosService) {}
+  constructor(
+    private turnosService: TurnosService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.obtenerTurnos();
   }
 
-
+    // OBTENER Y ORDENAR TURNOS
   obtenerTurnos() {
     let json = {
       id_turno: 0,
@@ -61,23 +67,16 @@ export class CatalogoTurnosComponent implements OnInit {
   }
 
 
-  onCheckboxChange(event: any) {
-    // Si el checkbox está marcado, establece isChecked en 1; de lo contrario, en 0.
-    this.isChecked = event.checked ? 1 : 0;
-  }
+
   updateTraslapa() {
     if (this.turnoSeleccionado) {
       this.turnoSeleccionado.traslapa_turno = 1;
       this.turnoSeleccionado.rota_turno = 1;
-      this.turnoSeleccionado.dia.descanso = 1;
     } else {
       this.turnoSeleccionado.traslapa_turno = 0;
       this.turnoSeleccionado.rota_turno = 0;
-      this.turnoSeleccionado.dia.descanso = 0;
     }
   }
-
-
 
   // Creamos turno seleccionado
   nuevoturno() {
@@ -105,46 +104,41 @@ export class CatalogoTurnosComponent implements OnInit {
       0,
       detTurno
     );
-    this.editando = true;
-
-
-    // let json = this.turnoSeleccionado;
-
-    //  console.log('OK nuevo', json);
-    // this.turnosService.agregarTurnos(json).subscribe((resp) => {
-    //   if (resp.ok) {
-    //     Swal.fire('Exito', resp.message, 'success');
-    //   }
-    // });
+     this.editando = false;
+    this.newTurno = true;
   }
 
-
-
-  // --------------
-  turnoSeleccionado: any = '';
   editarTurno(turnoSeleccionado: any) {
-    // this.editando =false; // Indica que estás editando un turno existente
+     this.editando = true;
+    this.newTurno = false;
+
     this.turnoSeleccionado = {
       clave: turnoSeleccionado.value[0].clave,
       turno: turnoSeleccionado.value[0].turno,
+      torelancia: turnoSeleccionado.value[0].tolerancia,
+      tiempo_comidaa: turnoSeleccionado.value[0].tiempo_comida,
       turno_id: turnoSeleccionado.value[0].id_turno,
       traslapa_turno: turnoSeleccionado.value[0].traslapa_turno,
       rota_turno: turnoSeleccionado.value[0].rota_turno,
       detalle: turnoSeleccionado.value,
     };
-    console.log("Turno Selecionado=>>",this.turnoSeleccionado)
+     console.log('Turno Selecionado=>>', this.turnoSeleccionado);
   }
 
-  horaEntrada: any = '';
-  toggleEditingMode() {
-    if (this.editando) {
+  guardarNuevo() {
+    if (this.newTurno) {
       this.turno.clave = this.turnoSeleccionado.clave;
-      this.turno.turno = this.turnoSeleccionado.turno;
+      this.turno.tiempo_comida = this.turnoSeleccionado.tiempo_comida;
+      this.turno.tolerancia = this.turnoSeleccionado.tolerancia;
       this.turno.traslapa_turno = this.turnoSeleccionado.traslapa_turno;
       this.turno.rota_turno = this.turnoSeleccionado.rota_turno;
       this.turno.detalle = this.turnoSeleccionado.detalle;
-      this.turno.id_turno = this.turnoSeleccionado.turno_id;
-      // console.log("DetalleTurno=>>",this.turno.detalle)
+      this.turno.turno = this.turnoSeleccionado.turno;
+
+      this.turno.token = 'token';
+      this.turno.id_turno = 0;
+      this.turno.activo = 1;
+      //  console.log("DetalleTurno=>>",this.turno.detalle)
 
       for (const dia of this.turno.detalle) {
         const diaSeleccionado = this.turnoSeleccionado.detalle.find(
@@ -153,33 +147,63 @@ export class CatalogoTurnosComponent implements OnInit {
         if (diaSeleccionado) {
           dia.hr_entrada = diaSeleccionado.hr_entrada;
           dia.hr_salida = diaSeleccionado.hr_salida;
-          dia.descanso = diaSeleccionado.descanso;
+          dia.descanso = diaSeleccionado.descanso ? 1 : 0;
         }
       }
 
       this.guardar();
-      this.editando = false;
+      this.newTurno = false;
     } else {
-      // Activa el modo de edición.
-      this.editando = true;
-    }
-    this.obtenerTurnos();
 
+      // this.editando = true;
+    }
   }
 
-guardar(){
-   let json = this.turno;
+  turnoEditado(){
+    this.turno = new Turno(0, 5, '', '', 0, 0, 0, 0, 0, '', 1, 0, [
+      this.detalle,
+    ]);
+    this.editando=true
+    this.newTurno = false;
+    this.turno.clave = this.turnoSeleccionado.clave;
+    this.turno.tiempo_comida = this.turnoSeleccionado.tiempo_comida;
+    this.turno.tolerancia = this.turnoSeleccionado.tolerancia;
+    this.turno.traslapa_turno = this.turnoSeleccionado.traslapa_turno;
+    this.turno.rota_turno = this.turnoSeleccionado.rota_turno;
+    this.turno.detalle = this.turnoSeleccionado.detalle;
+    this.turno.turno = this.turnoSeleccionado.turno
 
-      this.turnosService.agregarTurnos(json).subscribe((resp) => {
-        if (resp.ok) {
-          console.log('OK');
-        }
-      });
-      console.log("==", json);
-}
+    this.turno.token = 'token';
+    this.turno.id_turno = this.turnoSeleccionado.turno_id;
+    this.turno.activo = 1;
+    // console.log("DetalleTurno=>>",this.turno.detalle)
 
+    for (const dia of this.turno.detalle) {
+      const diaSeleccionado = this.turnoSeleccionado.detalle.find(
+        (d: any) => d.dia === dia.dia
+      );
+      if (diaSeleccionado) {
+        dia.hr_entrada = diaSeleccionado.hr_entrada;
+        dia.hr_salida = diaSeleccionado.hr_salida;
+        dia.descanso = diaSeleccionado.descanso ? 1 : 0;
+      }
+    }
+    console.log('edit?', this.turnoSeleccionado);
+    //  this.guardar();
+    // Activa el modo de edición.
+  }
 
+  guardar() {
+    let json = this.turno;
 
+    this.turnosService.agregarTurnos(json).subscribe((resp) => {
+      if (resp.ok) {
+        Swal.fire('Exito', 'turno guardado', 'success');
+        this.obtenerTurnos();
+      }
+    });
+    console.log('Guardar ->', json);
+  }
 
   activar(turnoSeleccionado: any) {
     this.editando = false;
@@ -205,85 +229,11 @@ guardar(){
         this.turnosService.activarTurno(json).subscribe((resp) => {
           if (resp.ok) {
             Swal.fire('Exito', 'el turno fue desactivado', 'success');
+            this.obtenerTurnos();
           }
         });
-        console.log('json :>> ', json);
-        this.obtenerTurnos();
+        // console.log('json :>> ', json);
       }
     });
   }
-
-  // --------------------------- Sin usar
-
-  // agregarTurno(){
-  //   let json = {
-  //   id_turno: 0,
-  //   id_cliente: 5,
-  //   clave: this.turno.clave,
-  //   turno: this.turno.turno,
-  //   traslapa_turno: 0,
-  //   rota_turno: 0,
-  //   id_turno_rota: 0,
-  //   tolerancia: this.turno.tolerancia,
-  //   tiempo_comida: 0,
-  //   token: "012354SDSDS01",
-  //   activo: 1,
-  //   id_usuario: 1,
-  //   detalle: [
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 1,
-  //         hr_entrada: "07:00",
-  //         hr_salida: "13:00",
-  //         descanso: 0
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 2,
-  //         hr_entrada: "07:00",
-  //         hr_salida: "13:00",
-  //         descanso: 0
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 3,
-  //         hr_entrada: "07:00",
-  //         hr_salida: "13:00",
-  //         descanso: 0
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 4,
-  //         hr_entrada: "09:00",
-  //         hr_salida: "17:00",
-  //         descanso: 0
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 5,
-  //         hr_entrada: "09:00",
-  //         hr_salida: "17:00",
-  //         descanso: 0
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 6,
-  //         hr_entrada: "09:00",
-  //         hr_salida: "17:00",
-  //         descanso: 1
-  //     },
-  //     {
-  //         id_det_turno: 0,
-  //         dia: 7,
-  //         hr_entrada: "7:00",
-  //         hr_salida: "17:00",
-  //         descanso: 1
-  //     }
-  // ]
-  //   }
-  //   console.log('=>',json);
-  // }
-
-
-
 }
