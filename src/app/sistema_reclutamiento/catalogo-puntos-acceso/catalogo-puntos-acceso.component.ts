60fbd3de7cbb30570277aaed68e95310b540d4a6
Edy <94276786+EdySnakee@users.noreply.github.com>
2023-11-14 13:56:18 -0600

    detalles de puntos de acceso

--- src/app/sistema_reclutamiento/catalogo-puntos-acceso/catalogo-puntos-acceso.component.ts
+++ src/app/sistema_reclutamiento/catalogo-puntos-acceso/catalogo-puntos-acceso.component.ts
@@ -1,10 +1,11 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { COLOR } from 'src/config/config';
 import Swal from 'sweetalert2';
 import { PAcceso } from 'src/app/models/PuntosAcceso';
 import { AccesosService } from 'src/app/services/puntos-acceso/accesos.service';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-catalogo-puntos-acceso',
@@ -15,7 +16,7 @@ export class CatalogoPuntosAccesoComponent implements OnInit {
   public cliente = window.sessionStorage.getItem("cliente");
   public usuario = parseInt(window.sessionStorage.getItem("user")+"");
   public sistema = parseInt(window.sessionStorage.getItem("sistema")+"");
-
+  @ViewChild('tabGroup') tabGroup!: MatTabGroup;
 
 public punto_acceso = new PAcceso(0,0,'','','','',0,'',0)
 public color = COLOR;
@@ -37,7 +38,7 @@ obtenerAccesos(){
     id_reloj: 0,
     id_cliente: this.cliente,
     ubicacion: "",
-    solo_activos: 1,
+    solo_activos: 0,
     token: "012354SDSDS01"
   }
   this.accesos_service.consultarAccesos(json).subscribe(res => {
@@ -81,6 +82,7 @@ obtenerAccesos(){
     Swal.fire({
       title: 'Guardando punto de acceso',
       text: 'Por favor espere...',
+      icon: 'info',
       willOpen: () => {
         Swal.showLoading();
       },
@@ -101,10 +103,18 @@ obtenerAccesos(){
     }
     this.accesos_service.guardarAccesos(json).subscribe(res =>{
       if(res.ok){
+        Swal.fire(
+          'Exito',
+          res.data.mensaje,
+          'success'
+        )
+        this.punto_acceso = new PAcceso(0,0,'','','','',0,'',0)
+       this.obtenerAccesos();
+       Swal.close();
+       console.log('json :>> ', res);
 
-        console.log('json :>> ', res);
       }else {
-        Swal.close();
+       this.obtenerAccesos();
         Swal.fire('error',res.message,'error');
       }
     })
@@ -113,18 +123,73 @@ obtenerAccesos(){
   autorizar(a:any){
     console.log('a :>> ', a);
     Swal.fire({
-      title: `¿Editar ${a.ubicacion}?`,
+      title: `¿${a.activo === 0 ? 'Autorizar' : 'Desactivar'} ${a.ubicacion}?`,
       text: '',
-      icon: 'info',
+      icon: 'question',
       showCancelButton: true,
-      confirmButtonText: 'Sí, editar',
+      confirmButtonText: 'Confirmar',
     }).then((result) => {
       if (result.isConfirmed) {
-        console.log('Ubicación editada');
+    let json = {
+      id_reloj: a.id_reloj,
+      token: 'token',
+      id_usuario: 1
+    }
+    this.accesos_service.activarAccesos(json).subscribe(res=>{
+      if(res.ok){
+        Swal.fire('', 'PUNTO DE ACCESO DESACTIVADO', 'success')
+        this.obtenerAccesos();
+      }else{
+        Swal.fire(res.message,'','error')
+        // console.log('Ubicación editada',res);
+      }
+    })
       } else {
-        console.log('Edición cancelada');
+        // console.log('Edición cancelada');
       }
     });
+  }
+
+  editar(a:any){
+   this.punto_acceso.ubicacion = a.ubicacion
+   this.longitud = a.longitud
+   this.latitud = a.latitud
+   this.punto_acceso.direccion_ip = a.direccion_ip
+   this.punto_acceso.distancia_km = a.distancia_km
+   this.cambiartab(1);
+   let json = {
+    id_reloj:a.id_reloj,
+    id_cliente:this.cliente,
+    ubicacion:this.punto_acceso.ubicacion,
+    latitud:this.latitud,
+    longitud:this.latitud,
+    direccion_ip:this.punto_acceso.direccion_ip,
+    distancia_km:this.punto_acceso.distancia_km,
+    token:'token',
+    id_usuario:0
+    }
+    // this.accesos_service.guardarAccesos(json).subscribe(res =>{
+    //   if(res.ok){
+    //     Swal.fire(
+    //       'Exito',
+    //       res.data.mensaje,
+    //       'success'
+    //     )
+    //     this.punto_acceso = new PAcceso(0,0,'','','','',0,'',0)
+    //    this.obtenerAccesos();
+    //    Swal.close();
+    //    console.log('json :>> ', res);
+
+    //   }else {
+    //    this.obtenerAccesos();
+    //     Swal.fire('error',res.message,'error');
+    //   }
+    // })
+    console.log(json);
+  }
+
+  cambiartab(nuevoIndice: number) {
+    this.tabGroup.selectedIndex = nuevoIndice;
   }
 
 }
